5bb658a51690764251d882bd11047fbeb880d5a6
David Romero <geekzolanos@gmail.com>
2020-03-31 09:37:25 -0400

    Reduccion en peso final de build

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-template-curly-in-string */
+const { fixBabelImports, override } = require('customize-cra');
+
+module.exports = override(
+    fixBabelImports("core", {
+        libraryName: '@material-ui/core',
+        libraryDirectory: 'esm',
+        camel2DashComponentName: false
+    }),
+    fixBabelImports("icons", {
+        libraryName: '@material-ui/icons',
+        libraryDirectory: 'esm',
+        camel2DashComponentName: false
+    })
+);
