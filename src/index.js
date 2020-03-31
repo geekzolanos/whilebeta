02cd1fc69944ab1076530270c59d4aab07a79875
David Romero <geekzolanos@gmail.com>
2020-03-31 09:37:54 -0400

    Implementacion de disableReactDevTools

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ import { render } from 'react-dom';
 import App from './app';
 import './assets/css/style.css';
 
+const disableReactDevtools = () => {
+    if (!window.__REACT_DEVTOOLS_GLOBAL_HOOK__) return;
+
+    for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+            window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = 
+            typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] == 'function'
+            ? Function.prototype
+            : null;
+    }
+}
+
+if (process.env.NODE_ENV === 'production')
+    disableReactDevtools();
+    
 render(<App />, document.getElementById('root'));
