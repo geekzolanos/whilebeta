12587579920abfa9da42532afec9e456ebdbfc0e
David Romero <geekzolanos@gmail.com>
2020-04-28 20:49:50 -0400

    Correcciones menores

--- src/components/ListTopicItem/ListTopicItem.jsx
+++ src/components/ListTopicItem/ListTopicItem.jsx
@@ -5,7 +5,7 @@ import { withStyles } from '@material-ui/core/styles';
 import styles from 'assets/jss/components/ListTopicItem/ListTopicItem';
 
 const CreatedThreshold = 259200;
-const isNew = topic => (CreatedThreshold >= topic.createdAt.seconds);
+const isNew = topic => ((Date.now() - topic.createdAt.toMillis()) <= CreatedThreshold * 1000);
 
 export default withStyles(styles)(({classes, value}) => (
     <ListItem button divider className={classes.root}>
@@ -16,7 +16,7 @@ export default withStyles(styles)(({classes, value}) => (
             <Grid item sm>
                 <Typography variant="h6" gutterBottom>{value.name}</Typography>
                 <div className={classes.chipArray}>
-                    { isNew(value) && <Chip className={classes.colorSuccess} size="small" color="primary" icon={<NewReleases />} label="Nuevo" /> }
+                    { !!isNew(value) && <Chip className={classes.colorSuccess} size="small" color="primary" icon={<NewReleases />} label="Nuevo" /> }
                     <Chip variant="outlined" size="small" icon={<WatchLater />} label={`${value.length} Min`} />
                 </div>
                 <Typography variant="body2" align="justify">{value.description}</Typography>
